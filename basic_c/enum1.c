e5d5b6d778c39d2070f9323ee6747529db2e9f5f
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-12-16 17:26:25 +0530

    add

---
+++ basic_c/enum1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+enum Colors { RED = 1, GREEN = 3, BLUE = 5 };
+
+int main() {
+    printf("The value of RED: %d\n", RED);      // Output: 1
+    printf("The value of GREEN: %d\n", GREEN);  // Output: 3
+    printf("The value of BLUE: %d\n", BLUE);    // Output: 5
+    return 0;
+}
+/*
+hello i am Tejas Falke and i am learning basic c programming language.
+*/
---
+++ basic_c/enum2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+enum WeekDays = { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAR, SATURDAY, SUNDAY};
+
+int main(){
+    enum WeekDays today = TUESDAY;
+
+    printf("The value of tueday: %d\n", today);
+    return 0;
+}
